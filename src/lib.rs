1636441ea82a890febebb370f8c998f33d34d041
Troy Neubauer <troyneubauer@gmail.com>
2021-11-19 16:25:14 -0500

    Remove old impls for Command get_pyro

--- src/lib.rs
+++ src/lib.rs
@@ -24,8 +24,8 @@ impl Into<usize> for StateIndex {
 ///
 #[derive(Debug, Serialize, Deserialize)]
 pub struct State {
-    pub name: String<32>,
-    pub checks: Vec<Check, 4>,
+    pub name: String<16>,
+    pub checks: Vec<Check, 3>,
     pub commands: Vec<Command, 1>,
     pub timeout: Option<Timeout>,
 }
@@ -39,32 +39,30 @@ pub struct Timeout {
 /// A check within a state that is run every time the state is run
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Check {
-    pub name: String<20>,
+    pub name: String<16>,
     pub check: CheckType,
     pub condition: CheckCondition,
     pub value: f32,
-    pub satisfied: CheckSatisfied,
+    pub on_satisfied: CheckSatisfied,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-#[allow(non_camel_case_types)]
 pub enum CheckType {
-    altitude,
-    pyro1_continuity,
-    pyro2_continuity,
-    pyro3_continuity,
+    Altitude,
+    Pyro1Continuity,
+    Pyro2Continuity,
+    Pyro3Continuity,
 }
 
 /// Represents a type of state check
 #[derive(Debug, Serialize, Deserialize)]
-#[allow(non_camel_case_types)]
 pub enum CheckCondition {
-    flag_set,
-    flag_unset,
-    equals { value: f32 },
-    greater_than { value: f32 },
-    less_than { value: f32 },
-    between { upper_bound: f32, lower_bound: f32 },
+    FlagSet,
+    FlagUnset,
+    Equals { value: f32 },
+    GreaterThan { value: f32 },
+    LessThan { value: f32 },
+    Between { upper_bound: f32, lower_bound: f32 },
 }
 
 /// A state transition due to a check being satisfied
@@ -79,102 +77,29 @@ pub enum CheckSatisfied {
     Abort(StateIndex),
 }
 
-#[allow(non_camel_case_types)]
 #[derive(Debug, Serialize, Deserialize, Copy, Clone)]
 pub enum CommandObject {
-    pyro1,
-    pyro2,
-    pyro3,
-    beacon,
-    data_rate,
-}
-
-#[derive(Debug, Serialize, Deserialize, Copy, Clone)]
-pub enum CommandObjectValueType {
-    Float32,
-    Int32,
-    Bool,
-    GPIO,
-}
-
-#[derive(Debug, Serialize, Deserialize, Copy, Clone)]
-pub enum CommandObjectValue {
-    Float32(f32),
-    Int32(i32),
-    Bool(bool),
-    GPIO(bool),
+    Pyro1,
+    Pyro2,
+    Pyro3,
+    Beacon,
+    DataRate,
 }
 
 /// A check within a state that is run every time the state is run
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Command {
     object: CommandObject,
-    value: CommandObjectValue,
+    value: f32,
     delay: f32,
-}
-
-//Using fail instead of a bunch of calls to unreachable! or panic!
-//leads to a much smaller code size
-#[cold]
-fn fail() -> ! {
-    panic!("Tried to take object of wrong type!");
 }
 
 impl Command {
     pub fn get_pyro(&self) -> bool {
-        match self.object {
-            CommandObject::pyro1 | CommandObject::pyro2 | CommandObject::pyro3 => {
-                match self.value {
-                    CommandObjectValue::GPIO(v) => v,
-                    _ => fail(),
-                }
-            }
-            _ => fail(),
-        }
+        self.value == 1.0
     }
 
     pub fn get_beacon(&self) -> bool {
-        match self.object {
-            CommandObject::beacon => match self.value {
-                CommandObjectValue::Bool(v) => v,
-                _ => fail(),
-            },
-            _ => fail(),
-        }
-    }
-
-    pub fn get_data_rate(&self) -> i32 {
-        match self.object {
-            CommandObject::beacon => match self.value {
-                CommandObjectValue::Int32(v) => v,
-                _ => fail(),
-            },
-            _ => fail(),
-        }
-    }
-}
-
-impl CommandObjectValue {
-    /// Returns the type of the contained value
-    pub fn classify(&self) -> CommandObjectValueType {
-        match *self {
-            CommandObjectValue::Float32(_) => CommandObjectValueType::Float32,
-            CommandObjectValue::Int32(_) => CommandObjectValueType::Int32,
-            CommandObjectValue::Bool(_) => CommandObjectValueType::Bool,
-            CommandObjectValue::GPIO(_) => CommandObjectValueType::GPIO,
-        }
-    }
-}
-
-impl CommandObject {
-    /// Returns the storage type of the given `CommandObject`
-    pub fn get_type(&self) -> CommandObjectValueType {
-        match *self {
-            CommandObject::pyro1 => CommandObjectValueType::GPIO,
-            CommandObject::pyro2 => CommandObjectValueType::GPIO,
-            CommandObject::pyro3 => CommandObjectValueType::GPIO,
-            CommandObject::beacon => CommandObjectValueType::Bool,
-            CommandObject::data_rate => CommandObjectValueType::Int32,
-        }
+        self.value == 1.0
     }
 }

0f4e2b36a7570193b7593b3f0c813ab28fb71dce
Troy Neubauer <troyneubauer@gmail.com>
2022-04-15 22:32:01 -0400

    Flash works with new lib on rev1

--- flight/src/main.rs
+++ flight/src/main.rs
@@ -1,7 +1,6 @@
 #![no_std]
 #![no_main]
 #![deny(unsafe_op_in_unsafe_fn)]
-#![feature(adt_const_params)]
 
 use core::cell::UnsafeCell;
 use core::fmt::Write;
@@ -10,6 +9,7 @@ use core::mem::MaybeUninit;
 use embedded_hal::digital::v2::OutputPin;
 use embedded_hal::spi::{Mode, Phase, Polarity};
 use hal::pac::USART2;
+use ms5611_spi::{Ms5611, Oversampling};
 
 use crate::hal::{pac, prelude::*, spi};
 use cortex_m_rt::entry;
@@ -85,17 +85,37 @@ fn main() -> ! {
     // ALTIMETER PC5
     // HIGH_G/ACCEL PB2
     //
+    let sck1 = gpioa.pa5.into_alternate();
+    let miso1 = gpioa.pa6.into_alternate();
+    let mosi1 = gpioa.pa7.into_alternate();
 
-    let sck = gpioc.pc10.into_alternate();
-    let miso = gpioc.pc11.into_alternate();
-    let mosi = gpioc.pc12.into_alternate();
+    let baro_cs = gpioc.pc5.into_push_pull_output();
+    let gyro_accel_cs = gpiob.pb0.into_push_pull_output();
+    let gyro_cs = gpiob.pb1.into_push_pull_output();
+
+    let pin1 = (sck1, miso1, mosi1);
+
+    let spi1 = spi::Spi::new(
+        dp.SPI1,
+        pin1,
+        Mode {
+            polarity: Polarity::IdleLow,
+            phase: Phase::CaptureOnFirstTransition,
+        },
+        1000.kHz(),
+        &clocks,
+    );
+
+    let sck3 = gpioc.pc10.into_alternate();
+    let miso3 = gpioc.pc11.into_alternate();
+    let mosi3 = gpioc.pc12.into_alternate();
     let flash_cs = gpiob.pb13.into_push_pull_output();
 
-    let pins = (sck, miso, mosi);
+    let pins3 = (sck3, miso3, mosi3);
 
-    let spi = spi::Spi::new(
+    let spi3 = spi::Spi::new(
         dp.SPI3,
-        pins,
+        pins3,
         Mode {
             polarity: Polarity::IdleLow,
             phase: Phase::CaptureOnFirstTransition,
@@ -114,45 +134,66 @@ fn main() -> ! {
     delay.delay_ms(100u32);
 
     println!("Initializing flash chip");
-    let flash = w25n512gv::new(spi, flash_cs)
+    let flash = w25n512gv::new(spi3, flash_cs)
         .map_err(|e| {
             println!("Flash chip failed to intialize. {e:?}");
         })
         .unwrap();
 
-    let (spi, flash_cs) = flash.reset(&mut delay);
+    let (spi3, flash_cs) = flash.reset(&mut delay);
 
-    let mut flash = w25n512gv::new(spi, flash_cs /*, &mut delay*/)
+    let mut flash = w25n512gv::new(spi3, flash_cs /*, &mut delay*/)
         .map_err(|e| {
             println!("Flash chip failed to intialize. {e:?}");
         })
         .unwrap();
 
     flash.modify_configuration_register(|r| {
-        r.modify(
-            w25n512gv::regs::Configuration::ECC_E::SET + w25n512gv::regs::Configuration::H_DIS::SET,
-        )
+        *r |= 1 << 4; // Enable ECC
+        *r |= 1; // disable HOLD
     });
 
-    // config_val |= 1 << 4; // Enable ECC
-    // config_val |= 1; // disable HOLD
-    // flash
-    //     .write_register(Addresses::CONFIGURATION_REGISTER, config_val)
-    //     .unwrap();
-
     // Disable all protections
-    flash.modify_protection_register(|r| r.set(0));
+    flash.modify_protection_register(|r| *r = 0);
+
+    println!("Starting initialization.");
+
+    let spi1_bus = shared_bus::BusManagerSimple::new(spi1);
+
+    let mut ms6511 = Ms5611::new(spi1_bus.acquire_spi(), baro_cs, &mut delay)
+        .map_err(|_| {
+            println!("Barometer failed to initialize.");
+        })
+        .unwrap();
+
+    println!("Barometer initialized.");
+
+    let mut bmi088_accel = bmi088::Builder::new_accel_spi(spi1_bus.acquire_spi(), gyro_accel_cs);
+
+    if let Err(_) = bmi088_accel.setup(&mut delay) {
+        println!("Low-G accelerometer failed to initialize.");
+        panic!();
+    }
+
+    println!("Low-G accelerometer initialized.");
+
+    let mut bmi088_gyro = bmi088::Builder::new_gyro_spi(spi1_bus.acquire_spi(), gyro_cs);
+
+    if let Err(_) = bmi088_gyro.setup(&mut delay) {
+        println!("Gyro failed to initialize.");
+        panic!();
+    }
 
     println!("Initialized.");
 
-    let test_page = 128;
+    let test_page = 7;
 
     println!("Persistent data from last time");
 
     let mut page = [0u8; w25n512gv::PAGE_SIZE_WITH_ECC];
     let mut r = flash.read_sync(test_page).unwrap();
-    dump_buf(&mut r, &mut page, 64);
-    let flash = r.finish().unwrap();
+    //dump_buf(&mut r, &mut page, 64);
+    let flash = r.finish();
 
     println!("Erasing chip...");
     let flash = flash.enable_write().unwrap();
@@ -161,8 +202,8 @@ fn main() -> ! {
     println!("page 0 after erase");
 
     let mut r = flash.read_sync(test_page).unwrap();
-    dump_buf(&mut r, &mut page, 64);
-    let flash = r.finish().unwrap().enable_write().unwrap();
+    //dump_buf(&mut r, &mut page, 64);
+    let flash = r.finish();
 
     println!("writing first time");
     let mut index: u8 = 0;
@@ -173,10 +214,10 @@ fn main() -> ! {
     });
 
     let r = flash.upload_to_buffer_sync(&test_data).unwrap();
-    let flash = r.commit_sync(test_page).unwrap().finish().unwrap();
+    let flash = r.commit_sync(test_page).unwrap().finish();
 
-    fn dump_buf<SPI, CS, const W: w25n512gv::Writability, const M: w25n512gv::BufMode>(
-        r: &mut BufferRef<SPI, CS, W, M>,
+    /*fn dump_buf<SPI, CS>(
+        r: &mut impl BufferRef<SPI, CS, stm32f4xx_hal::spi::Error, stm32f4xx_hal::spi::Error>,
         page: &mut [u8; w25n512gv::PAGE_SIZE_WITH_ECC],
         len: usize,
     ) where
@@ -194,11 +235,12 @@ fn main() -> ! {
         }
         println!();
     }
+    */
 
     println!("after 2 increment write");
     let mut r = flash.read_sync(test_page).unwrap();
-    dump_buf(&mut r, &mut page, 16);
-    let flash = r.finish().unwrap();
+    //dump_buf(&mut r, &mut page, 16);
+    let flash = r.finish();
 
     let mut index: u8 = 0;
     let test_data = [0u8; w25n512gv::PAGE_SIZE_WITH_ECC].map(|_| {
@@ -213,21 +255,21 @@ fn main() -> ! {
 
     println!("writing second time");
     let mut r = flash.upload_to_buffer_sync(&test_data).unwrap();
-    let flash = r.commit_sync(test_page).unwrap().finish().unwrap();
+    let flash = r.commit_sync(test_page).unwrap().finish();
 
     println!("after normal write");
 
     let mut r = flash.read_sync(test_page).unwrap();
-    dump_buf(&mut r, &mut page, 16);
-    let flash = r.finish().unwrap();
+    //dump_buf(&mut r, &mut page, 16);
+    let flash = r.finish();
 
     let mut r = flash.read_sync(test_page).unwrap();
-    dump_buf(&mut r, &mut page, 16);
-    let flash = r.finish().unwrap();
+    //dump_buf(&mut r, &mut page, 16);
+    let flash = r.finish();
 
     let mut r = flash.read_sync(test_page).unwrap();
-    dump_buf(&mut r, &mut page, 16);
-    let flash = r.finish().unwrap();
+    //dump_buf(&mut r, &mut page, 16);
+    let flash = r.finish();
 
     println!("OK");
     loop {}
